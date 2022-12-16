dd962fb593d2f644945626e9dba18a6965411f84
gabrielmj23 <gabrielmj2312@gmail.com>
2022-12-16 17:16:25 -0400

    More styling for appbar and presentation card

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { AppBar, Box, Button, Container, Grid, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Container, Grid, Paper, Typography } from '@mui/material';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <AppBar position='fixed' className='mui_navbar' sx={{ bgcolor: '#defff3', fontFamily: 'Oxygen' }}>
+      <AppBar position='fixed' className='mui_navbar' sx={{ bgcolor: '#defff3' }}>
         <Container maxWidth='xl' spacing={4}>
-          <Toolbar disableGutters>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} textAlign={'center'}>
-              <Typography component='a' href='#productos'>
-                <Button sx={{ my: 2, color: 'black', display: 'block' }}>Productos</Button>              
-              </Typography>
-              <Typography component='a' href='#mision'>
-                <Button sx={{ my: 2, color: 'black', display: 'block' }}>Mision</Button>
-              </Typography>
-              <Typography component='a' href='#nosotros'>
-                <Button sx={{ my: 2, color: 'black', display: 'block' }}>Sobre nosotros</Button>
-              </Typography>
-            </Box>
-          </Toolbar>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} textAlign={'center'}>
+            <Typography component='a' href='#productos' className='appbar-link' sx={{ my: 3, fontFamily: 'Nunito' }}>
+              PRODUCTOS
+            </Typography>
+            <Typography component='a' href='#mision' className='appbar-link' sx={{ my: 3, fontFamily: 'Nunito' }}>
+              MISION
+            </Typography>
+            <Typography component='a' href='#nosotros' className='appbar-link' sx={{ my: 3, fontFamily: 'Nunito' }}>
+              SOBRE NOSOTROS
+            </Typography>
+          </Box>
         </Container>
       </AppBar>
-      <Grid container spacing={3} maxWidth={'100%'} mx={3}>
-        <Grid item xs={8}>
-          <h1>Come saludable y responsable, <b>sin salir de tu casa</b></h1>
-        </Grid>
-        <Grid item xs={4}>
-          <img src='' alt='Foto relativa a la marca' />
-        </Grid>
-      </Grid>
+      <Box sx={{ width: '100%' }} display='flex' alignItems='center' justifyContent='center'>
+        <Paper elevation={8} sx={{ paddingBottom: 5, paddingTop: 5, width: '85%' }}>
+          <Grid container spacing={3}>
+            <Grid item xs={6} md={8}>
+              <Typography variant='h2' component='h1' align='center' sx={{ fontFamily: 'Montserrat' }}>
+                Come saludable y responsable,
+              </Typography>
+              <Typography variant='h3' component='h2' align='center' sx={{ fontFamily: 'Montserrat', fontStyle: 'italic' }}>
+                sin salir de tu casa
+              </Typography>
+            </Grid>
+            <Grid item xs={6} md={4}>
+              <img src='' alt='Foto relativa a la marca' />
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
     </div>
   )
 }
